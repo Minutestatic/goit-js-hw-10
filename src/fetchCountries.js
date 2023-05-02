b6229d28dee90987b82886cac5980e8b058a7f37
Maksym Lahutin <lagutinmaksim.98@gmail.com>
2023-05-02 22:30:27 +0300

    Добавлет поиск по странам

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,9 +1,9 @@
 const BASE_URL = 'https://restcountries.com/v3.1/name/';
 
-function fetchCountries() {
-  const URL = `${BASE_URL}${country}?fields=name,capital,population,flags,languages`;
+function fetchCountries(currentInputValue) {
+  const URL = `${BASE_URL}${currentInputValue}?fields=name,capital,population,flags,languages`;
   return fetch(URL).then(resp => {
-    if (!resp.ok) {
+    if (!resp.ok || resp.status === 404) {
       throw new Error(resp.statusText);
     }
     return resp.json();
@@ -11,7 +11,3 @@ function fetchCountries() {
 }
 
 export { fetchCountries };
-
-// fetchCountries()
-//   .then(data => console.log(data))
-//   .catch(err => console.log(err));
--- src/index.js
+++ src/index.js
@@ -11,4 +11,84 @@ const countryInfo = document.querySelector('.country-info');
 
 searchBox.addEventListener('input', debounce(onSearchInput, DEBOUNCE_DELAY));
 
-function onSearchInput(evt) {}
+function onSearchInput(evt) {
+  const currentInputValue = evt.target.value.trim();
+  if (!currentInputValue) {
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
+    return;
+  }
+  fetchCountries(currentInputValue)
+    .then(data => {
+      if (data.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      }
+      if (data.length >= 2 && data.length <= 10) {
+        countryList.innerHTML = citySearchResult(data);
+        countryInfo.innerHTML = '';
+      }
+      if (data.length === 1) {
+        countryList.innerHTML = '';
+        countryInfo.innerHTML = citiInfoResult(data);
+      }
+    })
+    .catch(err => {
+      countryList.innerHTML = '';
+      countryInfo.innerHTML = '';
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+      console.log(err);
+    });
+}
+
+function citySearchResult(countrys) {
+  return countrys
+    .map(
+      ({ name: { official }, flags: { svg, alt } }) =>
+        `<li class="country-iteam">
+          <img src="${svg}" alt="${alt}" width ="40">
+          <p>${official}</p>
+         </li>`
+    )
+    .join('');
+}
+
+function citiInfoResult(country) {
+  console.log(country);
+  return country
+    .map(
+      ({
+        name: { official },
+        flags: { svg, alt },
+        capital,
+        population,
+        languages,
+      }) =>
+        `
+        <ul>
+  <li class="country-iteam">
+    <img src="${svg}" alt="${alt}" width="100" />
+    <h1>${official}</h1>
+  </li>
+  <li class="country-iteam">
+    <p>
+      <span class="span-txt">Capital: </span>${capital}
+    </p>
+  </li>
+  <li class="country-iteam">
+    <p>
+      <span class="span-txt">Population: </span>${population}
+    </p>
+  </li>
+  <li class="country-iteam">
+    <p>
+      <span class="span-txt">languages: </span>${Object.values(languages).join(
+        ', '
+      )}
+    </p>
+  </li>
+</ul>`
+    )
+    .join('');
+}
